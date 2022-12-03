f25cb0a633582c486ed3b04f3bd8a519aff3415e
Michael van de Water <michael.vandewater@sharesies.co.nz>
2022-12-04 12:24:25 +1300

    Day 3, part b

---
+++ day-003/d3b.py
@@ -0,0 +1,29 @@
+from rich.console import Console
+from aocd import get_data, submit
+import time
+import string
+from enum import Enum, auto, IntEnum
+
+YEAR = 2022
+DAY = 3
+PART = "b"
+
+
+c = Console()
+start_time = time.perf_counter()
+data = get_data(year=YEAR, day=DAY)
+c.rule(f"Advent of Code {YEAR}-{DAY}{PART}")
+####################################################################################
+
+parsed = [i for i in data.strip().split("\n")]
+groups = [parsed[j:j + 3] for j in range(0, len(parsed), 3)]
+badges = [next(iter(set(k[0]) & set(k[1]) & set(k[2]))) for k in groups]
+priorities = [string.ascii_letters.index(k) + 1 for k in badges]
+
+answer = sum(priorities)
+print(answer)
+
+####################################################################################
+c.rule("Finished")
+
+submit(year=YEAR, day=DAY, part=PART, answer=answer)
